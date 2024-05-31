85ada1978f4fe4baab30e8ec841e6b1cd8ea8e83
jtaren <arenjatau@gmail.com>
2024-05-31 07:43:16 +0100

    Nav.jsx

--- my-page/src/components/nav/Nav.jsx
+++ my-page/src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import { FaUser } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { MdMiscellaneousServices } from "react-icons/md";
 import { MdMessage } from "react-icons/md";
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home"className='active' ><FaHome /></a>
-      <a href="#about"><FaUser /></a>
-      <a href="#experience"><FaBook /></a>
-      <a href="#services"><MdMiscellaneousServices /></a>
-      <a href="#contact"><MdMessage /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} ><FaHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdMiscellaneousServices /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdMessage /></a>
     </nav>
   )
 }
